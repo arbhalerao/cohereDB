24642fae3480d97eac0099d2747bec57c1d00ce8
Aditya Bhalerao <adityab7968@gmail.com>
2024-11-03 17:41:40 +0530

    Static Sharding (#1)
    * Introduce config.toml
    * Add server configs, config loader, and structs for TOML config
    * Implement Static Sharding
    * Fixed linter errors

--- web/server.go
+++ web/server.go
@@ -1,23 +1,30 @@
 package web
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"net/http"
 
 	"github.com/Aditya-Bhalerao/cohereDB/db"
+	"github.com/Aditya-Bhalerao/cohereDB/utils"
 )
 
 type Server struct {
-	db   *db.Database
-	addr string
+	db          *db.Database
+	addr        string
+	shardIdx    int
+	shardCount  int
+	serverAddrs *map[int]string
 }
 
-func NewServer(db *db.Database, addr string) *Server {
-	server := Server{
-		db:   db,
-		addr: addr,
+func NewServer(db *db.Database, addr string, shardIdx int, shardCount int, serverAddrs *map[int]string) *Server {
+	return &Server{
+		db:          db,
+		addr:        addr,
+		shardIdx:    shardIdx,
+		shardCount:  shardCount,
+		serverAddrs: serverAddrs,
 	}
-
-	return &server
 }
 
 // Method to register handlers
@@ -33,4 +40,49 @@ func (s *Server) RegisterHandlers() {
 // Method to start the server
 func (s *Server) Start() error {
 	return http.ListenAndServe(s.addr, nil)
+}
+
+// Hash the key and return the shard number
+func (s *Server) getShard(key string) int {
+	hash := sha256.Sum256([]byte(key))
+	return int(hash[0]) % s.shardCount
+}
+
+// ForwardRequest forwards an HTTP request to the specified target address and returns the response
+func (s *Server) ForwardRequest(targetAddr string, originalReq *http.Request) (*http.Response, error) {
+	forwardURL := fmt.Sprintf("http://%s%s", targetAddr, originalReq.URL.Path)
+
+	forwardReq, err := http.NewRequest(originalReq.Method, forwardURL, nil)
+	if err != nil {
+		utils.Logger.Error().Msgf("[FORWARD] Error creating forward request: %v", err)
+		return nil, fmt.Errorf("failed to create forward request: %v", err)
+	}
+
+	// Copy all headers from original request
+	for key, values := range originalReq.Header {
+		for _, value := range values {
+			forwardReq.Header.Add(key, value)
+		}
+	}
+
+	// Copy query parameters and form values
+	q := originalReq.URL.Query()
+	for key, values := range originalReq.Form {
+		for _, value := range values {
+			q.Add(key, value)
+		}
+	}
+	forwardReq.URL.RawQuery = q.Encode()
+
+	client := &http.Client{}
+
+	resp, err := client.Do(forwardReq)
+	if err != nil {
+		utils.Logger.Error().Msgf("[FORWARD] Error forwarding request to %s: %v", targetAddr, err)
+		return nil, fmt.Errorf("failed to forward request to %s: %v", targetAddr, err)
+	}
+
+	utils.Logger.Info().Msgf("[FORWARD] Successfully forwarded request to %s, status: %d", targetAddr, resp.StatusCode)
+
+	return resp, nil
 }
