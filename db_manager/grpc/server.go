1cc5d71a0d90633c46065731b3791d220b5ee7ce
Aditya Bhalerao <adityab7968@gmail.com>
2025-02-20 00:24:14 +0530

    Implement db_manager server

--- db_manager/grpc/server.go
+++ db_manager/grpc/server.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"fmt"
 	"net"
 
 	"github.com/arbha1erao/cohereDB/db_manager/grpc/pb"
@@ -53,19 +54,34 @@ func (s *Server) Stop() {
 }
 
 // Get retrieves the value for a given key from the database
-// ToDo
 func (s *Server) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, error) {
-	return &pb.GetResponse{Value: string("")}, nil
+	val, err := s.manager.GetKey(req.Key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get key %q: %w", req.Key, err)
+	}
+	return &pb.GetResponse{Value: val}, nil
 }
 
 // Set stores the provided key-value pair in the database
-// ToDo
 func (s *Server) Set(ctx context.Context, req *pb.SetRequest) (*pb.SetResponse, error) {
-	return &pb.SetResponse{Success: true}, nil
+	success, err := s.manager.SetKey(req.Key, req.Value)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set key %q: %w", req.Key, err)
+	}
+	if !success {
+		return nil, fmt.Errorf("failed to set key %q: operation unsuccessful", req.Key)
+	}
+	return &pb.SetResponse{Success: success}, nil
 }
 
 // Delete removes the key-value pair for the given key from the database
-// ToDo
 func (s *Server) Delete(ctx context.Context, req *pb.DeleteRequest) (*pb.DeleteResponse, error) {
-	return &pb.DeleteResponse{Success: true}, nil
+	success, err := s.manager.DeleteKey(req.Key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete key %q: %w", req.Key, err)
+	}
+	if !success {
+		return nil, fmt.Errorf("failed to delete key %q: operation unsuccessful", req.Key)
+	}
+	return &pb.DeleteResponse{Success: success}, nil
 }
