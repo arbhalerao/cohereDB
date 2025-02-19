b215187ac6fdd01dbc9b419a38e9dd01a6056cea
Aditya Bhalerao <adityab7968@gmail.com>
2025-02-19 14:03:08 +0530

    package pb

--- db_server/grpc/server.go
+++ db_server/grpc/server.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 
 	"github.com/arbha1erao/cohereDB/db"
+	"github.com/arbha1erao/cohereDB/db_server/grpc/pb"
 )
 
 type server struct {
-	UnimplementedDBServiceServer
+	pb.UnimplementedDBServiceServer
 	db *db.Database
 }
 
@@ -17,31 +18,31 @@ func NewServer(db *db.Database) *server {
 }
 
 // Get retrieves the value for a given key from the database
-func (s *server) Get(ctx context.Context, req *KeyRequest) (*KeyResponse, error) {
+func (s *server) Get(ctx context.Context, req *pb.KeyRequest) (*pb.KeyResponse, error) {
 	val, err := s.db.GetKey(req.Key)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get key '%s': %v", req.Key, err)
 	}
 
-	return &KeyResponse{Value: string(val)}, nil
+	return &pb.KeyResponse{Value: string(val)}, nil
 }
 
 // Set stores the provided key-value pair in the database
-func (s *server) Set(ctx context.Context, req *KeyValueRequest) (*SetResponse, error) {
+func (s *server) Set(ctx context.Context, req *pb.KeyValueRequest) (*pb.SetResponse, error) {
 	err := s.db.SetKey(req.Key, req.Value)
 	if err != nil {
 		return nil, fmt.Errorf("failed to set key '%s' with value '%s': %v", req.Key, req.Value, err)
 	}
 
-	return &SetResponse{Success: true}, nil
+	return &pb.SetResponse{Success: true}, nil
 }
 
 // Delete removes the key-value pair for the given key from the database
-func (s *server) Delete(ctx context.Context, req *KeyRequest) (*DeleteResponse, error) {
+func (s *server) Delete(ctx context.Context, req *pb.KeyRequest) (*pb.DeleteResponse, error) {
 	err := s.db.DeleteKey(req.Key)
 	if err != nil {
 		return nil, fmt.Errorf("failed to delete key '%s': %v", req.Key, err)
 	}
 
-	return &DeleteResponse{Success: true}, nil
+	return &pb.DeleteResponse{Success: true}, nil
 }
