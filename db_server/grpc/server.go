ecf7410973bbc5f9cd32f2a06b11a73b2e63a1bc
Aditya Bhalerao <adityab7968@gmail.com>
2025-04-30 23:33:16 +0530

    refactor(grpc): Improve gRPC server structure
    - Encapsulate gRPC server initialization and lifecycle in `Server` struct
    - Start() method to handle gRPC listener setup and server execution
    - Stop() method for graceful shutdown of the gRPC server
    - Rename from `db_server` to `grpc`

--- db_server/grpc/server.go
+++ db_server/grpc/server.go
@@ -1,24 +1,59 @@
-package db_server
+package grpc
 
 import (
 	"context"
 	"fmt"
+	"net"
 
 	"github.com/arbha1erao/cohereDB/db"
 	"github.com/arbha1erao/cohereDB/db_server/grpc/pb"
+	"github.com/arbha1erao/cohereDB/utils"
+	"google.golang.org/grpc"
 )
 
-type server struct {
+type Server struct {
 	pb.UnimplementedDBServiceServer
-	db *db.Database
+	db   *db.Database
+	grpc *grpc.Server
+	addr string
 }
 
-func NewServer(db *db.Database) *server {
-	return &server{db: db}
+func NewServer(db *db.Database, addr string) *Server {
+	grpcServer := grpc.NewServer()
+	s := &Server{
+		db:   db,
+		grpc: grpcServer,
+		addr: addr,
+	}
+	pb.RegisterDBServiceServer(grpcServer, s)
+	return s
+}
+
+// Start initializes the gRPC listener and starts the server.
+func (s *Server) Start() error {
+	listener, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		utils.Logger.Fatal().Err(err).Msg("Failed to start gRPC listener")
+		return err
+	}
+
+	utils.Logger.Info().Msgf("gRPC server listening on %s", s.addr)
+	if err := s.grpc.Serve(listener); err != nil {
+		utils.Logger.Fatal().Err(err).Msg("gRPC server failed")
+		return err
+	}
+
+	return nil
+}
+
+// Stop gracefully shuts down the gRPC server.
+func (s *Server) Stop() {
+	utils.Logger.Info().Msg("Shutting down gRPC server...")
+	s.grpc.GracefulStop()
 }
 
 // Get retrieves the value for a given key from the database
-func (s *server) Get(ctx context.Context, req *pb.KeyRequest) (*pb.KeyResponse, error) {
+func (s *Server) Get(ctx context.Context, req *pb.KeyRequest) (*pb.KeyResponse, error) {
 	val, err := s.db.GetKey(req.Key)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get key '%s': %v", req.Key, err)
@@ -28,7 +63,7 @@ func (s *server) Get(ctx context.Context, req *pb.KeyRequest) (*pb.KeyResponse, 
 }
 
 // Set stores the provided key-value pair in the database
-func (s *server) Set(ctx context.Context, req *pb.KeyValueRequest) (*pb.SetResponse, error) {
+func (s *Server) Set(ctx context.Context, req *pb.KeyValueRequest) (*pb.SetResponse, error) {
 	err := s.db.SetKey(req.Key, req.Value)
 	if err != nil {
 		return nil, fmt.Errorf("failed to set key '%s' with value '%s': %v", req.Key, req.Value, err)
@@ -38,7 +73,7 @@ func (s *server) Set(ctx context.Context, req *pb.KeyValueRequest) (*pb.SetRespo
 }
 
 // Delete removes the key-value pair for the given key from the database
-func (s *server) Delete(ctx context.Context, req *pb.KeyRequest) (*pb.DeleteResponse, error) {
+func (s *Server) Delete(ctx context.Context, req *pb.KeyRequest) (*pb.DeleteResponse, error) {
 	err := s.db.DeleteKey(req.Key)
 	if err != nil {
 		return nil, fmt.Errorf("failed to delete key '%s': %v", req.Key, err)
