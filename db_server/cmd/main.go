593cf414b9400793c521dd2ab819814b195f46fe
Aditya Bhalerao <adityab7968@gmail.com>
2025-05-01 20:43:56 +0530

    feat: Implement db server initialization
    - Initialize database instance based on the configured region
    - Initialize and Start both HTTP and gRPC servers concurrently
    - Implement graceful shutdown handling with OS signals
    
    This sets up the core server functionality, excluding manager registration.

--- db_server/cmd/main.go
+++ db_server/cmd/main.go
@@ -1,5 +1,98 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
+	"github.com/arbha1erao/cohereDB/db"
+	grpc_server "github.com/arbha1erao/cohereDB/db_server/grpc"
+	http_server "github.com/arbha1erao/cohereDB/db_server/http"
+	"github.com/arbha1erao/cohereDB/utils"
+)
+
+type Config struct {
+	Server struct {
+		Region       string `toml:"region"`
+		HTTP_Addr    string `toml:"http_addr"`
+		GRPC_Addr    string `toml:"grpc_addr"`
+		MANAGER_Addr string `toml:"manager_addr"`
+	} `toml:"server"`
+}
+
+func main() {
+	configPath := flag.String("config", "config.toml", "Path to the config file")
+	flag.Parse()
+
+	utils.NewLogger()
+	utils.Logger.Info().Msg("cohereDB server starting...")
+
+	// Load configuration
+	var config Config
+	err := utils.LoadTomlConfig(&config, *configPath)
+	if err != nil {
+		utils.Logger.Fatal().Err(err).Msg("Failed to load config file")
+		return
+	}
+
+	region := config.Server.Region
+	httpAddr := config.Server.HTTP_Addr
+	grpcAddr := config.Server.GRPC_Addr
+
+	// Initialize database
+	dbPath := fmt.Sprintf("../../data/db_%s", region)
+	database, err := db.NewDatabase(dbPath)
+	if err != nil {
+		utils.Logger.Fatal().Err(err).Msg("Failed to initialize database")
+		return
+	}
+	defer database.Close()
+
+	// Initialize HTTP Server
+	httpServer := http_server.NewServer(database, httpAddr)
+	httpServer.RegisterHandlers()
+
+	// Initialize gRPC Server
+	grpcService := grpc_server.NewServer(database, grpcAddr)
+
+	// Handle shutdown signals
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// Start HTTP server
+	go func() {
+		defer wg.Done()
+		utils.Logger.Info().Msgf("Starting HTTP server on %s", httpAddr)
+		if err := httpServer.Start(); err != nil {
+			utils.Logger.Fatal().Err(err).Msg("HTTP server failed")
+		}
+	}()
+
+	// Start gRPC server
+	go func() {
+		defer wg.Done()
+		utils.Logger.Info().Msgf("Starting gRPC server on %s", grpcAddr)
+		if err := grpcService.Start(); err != nil {
+			utils.Logger.Fatal().Err(err).Msg("gRPC server failed")
+		}
+	}()
+
+	<-stop
+	utils.Logger.Info().Msg("Shutting down servers...")
+
+	grpcService.Stop()
+
+	if err := httpServer.Shutdown(); err != nil {
+		utils.Logger.Fatal().Err(err).Msg("HTTP server shutdown error")
+	}
+
+	wg.Wait()
+
+	utils.Logger.Info().Msg("Servers stopped successfully.")
 }
