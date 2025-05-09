bbd0a6010bf644d3a7afc2c0e40400d784465da9
Aditya Bhalerao <adityab7968@gmail.com>
2025-05-09 23:57:04 +0530

    Remove `http` support; registration optional

--- db_server/cmd/main.go
+++ db_server/cmd/main.go
@@ -11,14 +11,12 @@ import (
 	"github.com/arbha1erao/cohereDB/db"
 	"github.com/arbha1erao/cohereDB/db_server/db_manager_client"
 	grpc_server "github.com/arbha1erao/cohereDB/db_server/grpc"
-	http_server "github.com/arbha1erao/cohereDB/db_server/http"
 	"github.com/arbha1erao/cohereDB/utils"
 )
 
 type Config struct {
 	Server struct {
 		Region       string `toml:"region"`
-		HTTP_Addr    string `toml:"http_addr"`
 		GRPC_Addr    string `toml:"grpc_addr"`
 		MANAGER_Addr string `toml:"manager_addr"`
 	} `toml:"server"`
@@ -26,6 +24,7 @@ type Config struct {
 
 func main() {
 	configPath := flag.String("config", "config.toml", "Path to the config file")
+	register := flag.Bool("register", false, "Indicates if registration should happen")
 	flag.Parse()
 
 	utils.NewLogger()
@@ -40,7 +39,6 @@ func main() {
 	}
 
 	region := config.Server.Region
-	httpAddr := config.Server.HTTP_Addr
 	grpcAddr := config.Server.GRPC_Addr
 	managerAddr := config.Server.MANAGER_Addr
 
@@ -51,41 +49,34 @@ func main() {
 		utils.Logger.Fatal().Err(err).Msg("Failed to initialize database")
 		return
 	}
-	defer database.Close()
-
-	// Initialize HTTP Server
-	httpServer := http_server.NewServer(database, httpAddr)
-	httpServer.RegisterHandlers()
+	defer func() {
+		if err := database.Close(); err != nil {
+			utils.Logger.Error().Err(err).Msg("Failed to close database")
+		}
+	}()
 
 	// Initialize gRPC Server
 	grpcService := grpc_server.NewServer(database, grpcAddr)
 
-	// Use DBManagerClient for registration
+	// Use DBManagerClient for registration, if registration flag is set
 	ready := make(chan bool)
-	managerClient := db_manager_client.NewDBManagerClient(managerAddr, region)
-	go managerClient.RegisterWithManager(region, httpAddr, grpcAddr, ready)
+	if *register {
+		managerClient := db_manager_client.NewDBManagerClient(managerAddr, region)
+		go managerClient.RegisterWithManager(region, grpcAddr, ready)
 
-	// Wait for registration to complete before proceeding
-	utils.Logger.Info().Msg("Waiting for registration with db_manager...")
-	<-ready
+		// Wait for registration to complete before proceeding
+		utils.Logger.Info().Msg("Waiting for registration with db_manager...")
+		<-ready
+		utils.Logger.Info().Msg("Registration successful. Starting servers...")
+	}
 	close(ready)
-	utils.Logger.Info().Msg("Registration successful. Starting servers...")
 
 	// Handle shutdown signals
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
 	var wg sync.WaitGroup
-	wg.Add(2)
-
-	// Start HTTP server
-	go func() {
-		defer wg.Done()
-		utils.Logger.Info().Msgf("Starting HTTP server on %s", httpAddr)
-		if err := httpServer.Start(); err != nil {
-			utils.Logger.Fatal().Err(err).Msg("HTTP server failed")
-		}
-	}()
+	wg.Add(1)
 
 	// Start gRPC server
 	go func() {
@@ -100,10 +91,6 @@ func main() {
 	utils.Logger.Info().Msg("Shutting down servers...")
 
 	grpcService.Stop()
-
-	if err := httpServer.Shutdown(); err != nil {
-		utils.Logger.Fatal().Err(err).Msg("HTTP server shutdown error")
-	}
 
 	wg.Wait()
 
